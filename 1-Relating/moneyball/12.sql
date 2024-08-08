f94ad7a6ab30f7eb8eb09b1f122376c6f99c495f
Vishal Singhania <vishsinghania@outlook.com>
2024-08-09 03:33:44 +0530

    new solution

--- 1-Relating/moneyball/12.sql
+++ 1-Relating/moneyball/12.sql
@@ -1,75 +1,46 @@
-with hit_cost as (
-    select
-        pf.player_id,
-        s.salary,
-        pf.H,
-        s.salary / pf.H as dollars_per_hit
-    from
-        salaries as s
-    join
-        performances as pf
-        on s.player_id = pf.player_id
-        and s.year = pf.year
-    where
-        s.year = 2001 and
-        pf.H > 0
+WITH per_hit AS (
+    SELECT
+        players.first_name,
+        players.last_name,
+        salaries.salary / performances.H AS "dollars per hit"
+    FROM
+        players
+        JOIN salaries ON players.id = salaries.player_id
+        JOIN performances ON players.id = performances.player_id
+        AND salaries.year = performances.year
+    WHERE
+        performances.year = 2001
+        AND performances.H > 0
+    ORDER BY
+        "dollars per hit"
+    LIMIT
+        10
 ),
 
-hit_cost_ranked as (
-    select
-        player_id,
-        dollars_per_hit,
-        rank() over (order by dollars_per_hit) as dollars_per_hit_rank
-    from
-        hit_cost
-),
-
-rbi_cost as (
-    select
-        pf.player_id,
-        s.salary,
-        pf.RBI,
-        s.salary / pf.RBI as dollars_per_rbi
-    from
-        salaries as s
-    join
-        performances as pf
-        on s.player_id = pf.player_id
-        and s.year = pf.year
-    where
-        s.year = 2001 and
-        pf.RBI > 0
-),
-
-rbi_cost_ranked as (
-    select
-        player_id,
-        dollars_per_rbi,
-        rank() over (order by dollars_per_rbi) as dollars_per_rbi_rank
-    from
-        rbi_cost
-),
-
-cheapest_players as (
-    select
-        hcr.player_id
-    from
-        hit_cost_ranked as hcr
-    inner join
-        rbi_cost_ranked as rcr
-        on hcr.player_id = rcr.player_id
-    where
-        hcr.dollars_per_hit_rank <= 10 and
-        rcr.dollars_per_rbi_rank <= 10
+per_rbi AS (
+    SELECT
+        players.first_name,
+        players.last_name,
+        salaries.salary / performances.RBI AS "dollars per rbi"
+    FROM
+        players
+        JOIN salaries ON players.id = salaries.player_id
+        JOIN performances ON players.id = performances.player_id
+        AND salaries.year = performances.year
+    WHERE
+        performances.year = 2001
+        AND performances.RBI > 0
+    ORDER BY
+        "dollars per rbi"
+    LIMIT
+        10
 )
 
-select
-    pl.first_name,
-    pl.last_name
-from
-    cheapest_players as cp
-inner join
-    players as pl
-    on cp.player_id = pl.id
-order by
-    pl.id;
+SELECT
+    per_rbi.first_name, per_rbi.last_name
+FROM
+    per_rbi
+JOIN
+    per_hit ON per_hit.first_name = per_rbi.first_name
+ORDER BY
+    per_rbi.last_name;
